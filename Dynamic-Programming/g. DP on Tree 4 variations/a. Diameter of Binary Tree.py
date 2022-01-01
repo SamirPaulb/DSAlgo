df0ced0b947c79107f8ade88fa5566689f7d6a72
Samir Paul <samir.paul.personal@gmail.com>
2022-01-01 13:35:06 +0530

    dp on tree

---
+++ Dynamic-Programming/g. DP on Tree 4 variations/a. Diameter of Binary Tree.py
@@ -0,0 +1,46 @@
+# https://www.youtube.com/watch?v=zmPj_Ee3B8c&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=47
+# https://leetcode.com/problems/diameter-of-binary-tree/
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        def solve(root):
+            # Base Case : Tree is empty
+            if not root: return 0
+            
+            l = solve(root.left)  # diameter of left subtree
+            r = solve(root.right) # diameter of right subtree
+            
+            temp = 1 + max(l, r)
+            
+            # update the answer, because diameter of a tree is nothing but maximum value of (left_height + right_height + 1) for each node
+            self.res = max(self.res, l + r)  # if current path is longer than previous then update res
+            
+            return temp  # returning temp insted of res; longest path may not include current node or left or right subtree
+        
+        solve(root)
+        return self.res
+
+    
+# Time Complexity: O(n) 
+# Space Complexity: O(1)
+
+'''
+Input :     1
+          /   \
+        2      3
+      /  \
+    4     5
+
+Output : 4
+
+
+Input :     1
+          /   \
+        2      3
+      /  \      \
+    4     5      6
+
+Output : 5
+
+'''
