39837f43138b368d515b011ccd5a83c9f94fc174
Samir Paul <samir.paul.personal@gmail.com>
2022-01-24 20:52:09 +0530

    --

--- 04_LinkedList/LRU Cache.py
+++ 04_LinkedList/LRU Cache.py
@@ -1,6 +1,75 @@
 # Using LinkedList and Dictionary  => Optimized for Interviews
+# https://youtu.be/akFRa58Svug
+# https://youtu.be/UudCSLREm08
 
+class Node(object):
+    def __init__(self, key, x):
+        self.key = key
+        self.value = x
+        self.next = None
+        self.prev = None
+    
+class LRUCache(object):
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity    
+        self.dic = {}
+        self.head = None
+        self.tail = None
 
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key in self.dic:
+            node = self.dic[key]
+            self.makeMostRecent(node)
+            return node.value
+        else:
+            return -1
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key not in self.dic:
+            # Invalidate oldest if required:
+            if len(self.dic) == self.capacity:
+                del self.dic[self.head.key]
+                self.removeHead()
+            node = Node(key, value)
+            self.dic[key] = node
+        else:
+            # Update the value:
+            node = self.dic[key]
+            node.value = value
+        self.makeMostRecent(node)
+            
+    def makeMostRecent(self, node):
+        if self.head and self.head.key == node.key:
+            self.removeHead()
+        # Make this item the most recently accessed (tail):
+        if self.tail and not self.tail.key == node.key:
+            if node.prev:
+                node.prev.next = node.next
+            if node.next:
+                node.next.prev = node.prev
+            node.next = None
+            self.tail.next = node  # Old tail's next is the node.
+            node.prev = self.tail  # Node's previous is the old tail.
+        self.tail = node  # New tail is the node.
+        if not self.head:
+            self.head = node  # If this is the first item make it the head as well as tail.
+    
+    def removeHead(self):
+        self.head = self.head.next
+        if self.head:
+            self.head.prev = None
 
 
 
