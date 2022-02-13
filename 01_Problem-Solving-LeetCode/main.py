55a191bbb05aa4d0c6013871a6225b9ce12c7e8b
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-02-13 19:28:10 +0530

    string

--- 01_Problem-Solving-LeetCode/main.py
+++ 01_Problem-Solving-LeetCode/main.py
@@ -1,7 +1,7 @@
 import os
 
 ## Number of days you want to make commits
-for i in range(1,165*2 + 1):
+for i in range(1,365*2 + 1):
     d = str(i) + '---'
     ## Open a text file and modify it
     with open('bot.txt', 'a') as file:
