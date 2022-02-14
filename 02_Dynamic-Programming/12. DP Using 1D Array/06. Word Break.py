2cda8c08156e526f0e88db08d00ad2b724000d69
Samir Paul <samir.paul.personal@gmail.com>
2022-02-14 18:53:13 +0530

    word break prolem leetcode

---
+++ 02_Dynamic-Programming/12. DP Using 1D Array/06. Word Break.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/word-break/
+# https://youtu.be/Sx9NNgInc3A
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        '''
+        # Brute Force
+        self.res = False
+        def solve(s):
+            if not s: 
+                self.res = True
+                return
+            for i in range(1, len(s)+1):
+                if s[:i] in wordDict:
+                    solve(s[i:])
+        
+        solve(s)
+        return self.res
+        '''
+        
+        # Dynamic Programming
+        n = len(s)
+        dp = [False]*(n+1)
+        dp[n] = True
+        
+        for i in range(n-1, -1, -1):
+            for word in wordDict:
+                if i + len(word) <= n and s[i:i+len(word)] == word:
+                    dp[i] = dp[i+len(word)]
+                    if dp[i] == True: 
+                        break
+        
+        return dp[0]
