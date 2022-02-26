e8a98e8ac5ea6f739e52e6c164817ead898f186b
Samir Paul <samir.paul.personal@gmail.com>
2022-02-26 19:41:12 +0530

    --

---
+++ 02_Dynamic-Programming/12. DP Using 1D Array/10. Delete and Earn.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/delete-and-earn/
+
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        if not nums: return 0
+        
+        arr = [0]*(max(nums)+1)
+        
+        for i in nums:
+            arr[i] += i
+        
+        dp = [0]*(len(arr))
+        
+        for i in range(len(arr)):
+            if i == 0:
+                dp[i] =  0
+            elif i == 1:
+                dp[i] = max(arr[i], dp[i-1])
+            else:
+                dp[i] = max(arr[i] + dp[i-2], dp[i-1])
+        
+        return dp[-1]
