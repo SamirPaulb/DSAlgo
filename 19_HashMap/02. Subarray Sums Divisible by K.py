66cbeb86e08fc9c68370adcfebde19267fe9ba74
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-03-04 21:47:22 +0530

    python dictionary

---
+++ 19_HashMap/02. Subarray Sums Divisible by K.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/subarray-sums-divisible-by-k/
+# https://www.youtube.com/watch?v=QM0klnvTQzk
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        # Use COncept of 523. Continuous Subarray Sum
+        
+        ans = 0
+        remainderCountDic = {0 : 1}
+        curSum = 0
+        
+        for i in range(len(nums)):
+            curSum += nums[i]
+            
+            if curSum < 0: curSum += k
+            
+            curSum = curSum 
+            
+            if curSum in remainderCountDic:
+                ans += remainderCountDic[curSum]
+                remainderCountDic[curSum] += 1
+            else:
+                remainderCountDic[curSum] = 1
+        
+        return ans
