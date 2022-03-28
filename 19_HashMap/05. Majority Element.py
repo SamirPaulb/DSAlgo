0b8b6cadbc313aa8f9f83e08143f7071bf0b9c83
Samir Paul <samir.paul.personal@gmail.com>
2022-03-28 17:17:36 +0530

    DSA

---
+++ 19_HashMap/05. Majority Element.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/majority-element/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        me = -1   # me = Majority Element
+        count = 0      # count = Count of Majority Element
+        
+        for i in range(len(nums)):
+            if nums[i] == me:
+                count += 1
+            elif count > 0 and nums[i] != me:
+                count -= 1
+            
+            if count == 0:
+                me = nums[i]
+                count = 1
+        
+        if nums.count(me) >= len(nums) // 2:
+            return me
+        return -1
