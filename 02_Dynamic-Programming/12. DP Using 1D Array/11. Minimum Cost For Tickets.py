fb8bdc48edc3f7c5c7f5330c6afe5fe90b068d1e
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-04-04 16:22:25 +0530

    dp

---
+++ 02_Dynamic-Programming/12. DP Using 1D Array/11. Minimum Cost For Tickets.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/minimum-cost-for-tickets/
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        n = max(days) + 1
+        dp = [-1 for x in range(n)]
+        #dp = [-1]*(max(days)+1)
+        dp[0] = 0
+        
+        for x in range(1,len(dp)):
+            if x in days:
+                if x>=30:
+                    #print(x,min(dp[x-1]+costs[0],dp[x-7]+costs[1],dp[x-30]+costs[2]))
+                    dp[x] = min(dp[x-1]+costs[0],dp[x-7]+costs[1],dp[x-30]+costs[2])
+                    
+                elif x>=7 and x<30:
+                    dp[x] = min(dp[x-1]+costs[0],dp[x-7]+costs[1],costs[2])
+                    
+                else:
+                    dp[x] = min(dp[x-1]+costs[0],costs[1],costs[2])
+                    
+            else:
+                dp[x] = dp[x-1]
+                
+        return dp[max(days)]
+        
