f9e3028c5186e44cce3fa4035d482f8b34201b11
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-04-05 19:43:55 +0530

    --

---
+++ 16_String/03. Longest Common Prefix.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/longest-common-prefix/
+# https://youtu.be/0sWShKIJoo4
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        for i in range(len(strs[0])):
+            for st in strs:
+                if i == len(st) or strs[0][i] != st[i]:
+                    return res
+            res += strs[0][i]
+        
+        return res
