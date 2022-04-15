55af5240ae1fbdcd2f46b87c9a53d2da3c2e0441
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-04-16 02:07:50 +0530

    --

---
+++ 15_Greedy/04. Queue Reconstruction by Height.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/queue-reconstruction-by-height/
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key = lambda x : (-x[0], x[1]))
+        res = []
+        
+        for ch in people:
+            res.insert(ch[1], ch)
+        
+        return res
+    
+# Time: O(N^2)
+# space: O(N)
