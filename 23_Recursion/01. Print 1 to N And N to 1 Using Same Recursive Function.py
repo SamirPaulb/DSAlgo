a5ddb32cfac271c85849c54d654cd4809474a0c5
Samir Paul <samir.paul.personal@gmail.com>
2022-05-04 12:08:33 +0530

    recursion

---
+++ 23_Recursion/01. Print 1 to N And N to 1 Using Same Recursive Function.py
@@ -0,0 +1,51 @@
+# print N to 1 using recursion
+def solve(n):
+    if n == 1: 
+        print(n)
+        return
+    print(n)  # print before calling the function
+    solve(n-1)
+    
+solve(7)
+''' 
+stack = [solve(7), solve(6), solve(5), solve(4), solve(3), solve(2), solve(1)]
+while returning there are noting so stack got popped and print nothing during returning
+'''
+
+
+
+print('------------------------------')
+
+
+# Print 1 to N using recursion
+def solve(n):
+    if n == 1:
+        print(n)
+        return
+    solve(n-1)
+    print(n)  # print after calling the function
+
+solve(7)
+''' 
+stack = [solve(7), solve(6), solve(5), solve(4), solve(3), solve(2), solve(1)]
+while appending there is nothing in to print, while returning there is print function 
+so stack got popped and get printted in reversed order
+'''
+
+print('------------------------------')
+
+
+def solve(n):
+    if n == 1: 
+        print(n)
+        return
+    print(n)  
+    solve(n-1)
+    print(n)
+    
+solve(7)
+''' 
+stack = [solve(7), solve(6), solve(5), solve(4), solve(3), solve(2), solve(1)]
+while appending to the stack print(n) and after returning from 1 stack got popped and print(n)
+again
+ '''
---
+++ 23_Recursion/readme.md
@@ -0,0 +1,10 @@
+## Recursion
+
+[Video Playlist](https://www.youtube.com/playlist?list=PL_z_8CaSLPWeT1ffjiImo0sYTcnLzo-wY)
+
+**Steps**:
+    1) Make input smaller (Find Base Condition - Smallest valid input)
+    2) Identify Recursion - Decision Space (Choise + Decission)
+    3) Recursion Tree - (main part of recursion)
+    4) Write the code
+
