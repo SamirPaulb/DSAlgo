e4fe5c471a6648ed84307cbbea6fef21e4e679f7
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-05-07 17:21:41 +0530

    merge sort

--- 03_Sorting-Algorithms/merge sort.py
+++ 03_Sorting-Algorithms/merge sort.py
@@ -1,3 +1,67 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def mergeSort(arr):
+    if len(arr) > 1:
+        #  mid is the point where the array is divided into two subarrays
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        # Sort the two halves
+        mergeSort(left)
+        mergeSort(right)
+
+        i = j = k = 0
+
+        # Until we reach either end of either left or right, pick larger among
+        # elements left and right and place them in the correct position at A[p..r]
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        # When we run out of elements in either left or right,
+        # pick up the remaining elements and put in A[p..r]
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+    return arr
+
+# Taking inpit using fast I/O.
+def takeInput() :
+    arr = list(map(int, stdin.readline().strip().split(" ")))
+    return arr
+
+# Main.
+arr = takeInput()
+print(mergeSort(arr))
+
+# Time: O(n log(n))    # log(n) for deviding into 2 parts and n for linear traversing
+# Space: O(n)          # as everytime we are taking new array left and right
+
+
+
+
+
+
+
+
+
+''' 
 # Merge Sort Algorithm
 
 a = [1,4,45,66,8,89,54,0,5,6,75,675,7,56]
@@ -39,3 +103,4 @@ mergesort(a, 0, len(a)-1)
 print(a)
 
 
+'''
