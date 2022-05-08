02e59d1aeb03a5f89591659dcc47db8480b47a18
Samir Paul <samir.paul.personal@gmail.com>
2022-05-08 20:26:25 +0530

    --

--- 03_Sorting-Algorithms/bubblesort.py
+++ 03_Sorting-Algorithms/bubblesort.py
@@ -1,11 +1,14 @@
 # Bubble Sort Algorithm
 
-a = [12,34,56,8,45,8,3,7,23,7,975,345,7,5,43]
+arr = [12,34,56,8,45,8,3,7,23,7,975,345,7,5,43]
 
-for i in range(len(a)):
-    for j in range(len(a)-i-1):
-        if a[j] > a[j+1]:
-            a[j], a[j+1] = a[j+1], a[j]
+for i in range(len(arr)):
+    for j in range(len(arr)-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
 
 
-print(len(a))
+print(arr)
+
+# Time: O(N^2)
+# Space: O(1)
