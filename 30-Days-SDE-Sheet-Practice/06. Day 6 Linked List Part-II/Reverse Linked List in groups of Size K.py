ae0d945112d7ad95b4c19eca147b455852add91a
Samir Paul <samir.paul.personal@gmail.com>
2022-05-10 22:34:09 +0530

    day 6 completed

--- 30-Days-SDE-Sheet-Practice/06. Day 6 Linked List Part-II/Reverse Linked List in groups of Size K.py
+++ 30-Days-SDE-Sheet-Practice/06. Day 6 Linked List Part-II/Reverse Linked List in groups of Size K.py
@@ -1,2 +1,31 @@
 # https://leetcode.com/problems/reverse-nodes-in-k-group/
+''' 
+Apply the concept of reverse LinkedList in a range of length K.
+'''
 
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+        dummy.next = head
+        cur = head
+        count = 0
+        while cur:
+            count += 1
+            cur = cur.next
+        
+        pre = dummy
+        while count >= k:
+            cur = pre.next
+            nex = cur.next
+            for i in range(k-1):
+                cur.next = nex.next
+                nex.next = pre.next
+                pre.next = nex
+                nex = cur.next
+            pre = cur
+            count -= k
+            
+        return dummy.next
+
+# Time: O(N)
+# Space: O(1)
