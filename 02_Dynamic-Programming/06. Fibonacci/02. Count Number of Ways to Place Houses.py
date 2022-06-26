169d302c62e432c6fe3f45206b4c56786f919d9e
Samir Paul <samir.paul.personal@egmail.com>
2022-06-26 14:09:09 +0530

    DP in fibo

--- 02_Dynamic-Programming/06. Fibonacci/02. Count Number of Ways to Place Houses.py
+++ 02_Dynamic-Programming/06. Fibonacci/02. Count Number of Ways to Place Houses.py
@@ -1,5 +1,9 @@
 # https://leetcode.com/problems/count-number-of-ways-to-place-houses/
 
+'''
+One side of street has no effect to the other. The number of way on one side is fibo sequence.
+return fibo * fibo in the end.
+'''
 class Solution:
     def countHousePlacements(self, n):
         a, b, mod = 1, 1, 10**9 + 7
@@ -9,4 +13,17 @@ class Solution:
 '''
 Time O(n)
 Space O(1)
+'''
+
+
+class Solution:
+    def countHousePlacements(self, n: int) -> int:
+        MOD = 10**9 + 7
+        dp = [1, 1] 
+        for i in range(2, n + 2) :
+            dp.append(dp[i-1] + dp[i-2])
+        return (dp[n+1] ** 2) % MOD
+'''
+Time O(n)
+Space O(n)
 '''
