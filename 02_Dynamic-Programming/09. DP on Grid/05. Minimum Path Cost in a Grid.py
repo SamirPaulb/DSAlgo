6270df954db3d13938f5f13bb4672f12dc3e92ac
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-06-28 21:29:17 +0530

    Update 05. Minimum Path Cost in a Grid.py

--- 02_Dynamic-Programming/09. DP on Grid/05. Minimum Path Cost in a Grid.py
+++ 02_Dynamic-Programming/09. DP on Grid/05. Minimum Path Cost in a Grid.py
@@ -4,6 +4,38 @@ class Solution:
     def minPathCost(self, grid: List[List[int]], moveCost: List[List[int]]) -> int:
         row = len(grid)
         col = len(grid[0])
+        '''
+        NOTE: Incase of matrix (2-D Array) Python's matrix.copy() function Does NOT make a new copy of original matrix. So any change in new_matrix will cange in original matrix.
+        '''
+        dp = self.deepcopy(grid)  # Can NOT use, dp = grid.copy()  or  dp = grid[:]
+        
+        for i in range(1, row):
+            for j in range(col):
+                val = 2**31
+                for k in range(col):
+                    val = min(val, dp[i-1][k] + moveCost[grid[i-1][k]][j])
+                dp[i][j] += val
+        
+        return min(dp[-1])
+    
+    def deepcopy(self, arr):
+        n = len(arr)
+        m = len(arr[0])
+        new_arr = [[0]*m for i in range(n)]
+        
+        for i in range(n):
+            for j in range(m):
+                new_arr[i][j] = arr[i][j]
+        
+        return new_arr
+
+    
+
+
+class Solution:
+    def minPathCost(self, grid: List[List[int]], moveCost: List[List[int]]) -> int:
+        row = len(grid)
+        col = len(grid[0])
         dp = [[0]*col for i in range(row)]
         
         for j in range(col):
