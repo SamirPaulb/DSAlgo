c26436668f392e99dbe181e2e982832530e8b2b5
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-06-30 15:31:51 +0530

    Update 07. Word Break II.py

--- 02_Dynamic-Programming/12. DP Using 1D Array/07. Word Break II.py
+++ 02_Dynamic-Programming/12. DP Using 1D Array/07. Word Break II.py
@@ -2,26 +2,25 @@
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        n = len(s)
-        dic = {i : [] for i in range(n+1)}
+        dic = {i : [] for i in range(len(s))}
         
-        for i in range(n-1, -1, -1):
+        for i in range(len(s)-1, -1, -1):
             for word in wordDict:
-                if i+len(word) <= n and s[i:i+len(word)] == word:
+                if i + len(word) <= len(s) and s[i:i+len(word)] == word:
                     dic[i].append(word)
         
-        if len(dic[0]) == 0: return []
-        
-        def solve(j, tmp):
-            if j >= n: 
-                res.append(tmp)
-            for i in range(len(dic[j])): 
-                solve(j+len(dic[j][i]), tmp + " " + dic[j][i])
-        
         res = []
-        for i in range(len(dic[0])):
-            j = len(dic[0][i])
-            tmp = dic[0][i]
-            solve(j, tmp)
+        if not dic[0]: return res
         
+        def solve(i, path):
+            if i >= len(s):
+                res.append(path)
+                return
+            for num in dic[i]:
+                if path == "":
+                    solve(i + len(num), num)
+                else:
+                    solve(i + len(num), path + " " + num)
+        
+        solve(0, "")
         return res
