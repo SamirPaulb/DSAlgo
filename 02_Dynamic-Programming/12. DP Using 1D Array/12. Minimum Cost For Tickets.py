972291dc9e35816a65e28851f5c33d7a9ab09f03
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-06-30 18:23:09 +0530

    Update 12. Minimum Cost For Tickets.py

--- 02_Dynamic-Programming/12. DP Using 1D Array/12. Minimum Cost For Tickets.py
+++ 02_Dynamic-Programming/12. DP Using 1D Array/12. Minimum Cost For Tickets.py
@@ -2,25 +2,25 @@
 
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        given_days = set(days)
         n = max(days) + 1
-        dp = [-1 for x in range(n)]
-        #dp = [-1]*(max(days)+1)
-        dp[0] = 0
+        dp = [0] * n
         
-        for x in range(1,len(dp)):
-            if x in days:
-                if x>=30:
-                    #print(x,min(dp[x-1]+costs[0],dp[x-7]+costs[1],dp[x-30]+costs[2]))
-                    dp[x] = min(dp[x-1]+costs[0],dp[x-7]+costs[1],dp[x-30]+costs[2])
-                    
-                elif x>=7 and x<30:
-                    dp[x] = min(dp[x-1]+costs[0],dp[x-7]+costs[1],costs[2])
-                    
+        for i in range(1, n):
+            if i in given_days:
+                if i >= 30: 
+                    dp[i] = min(dp[i-1] + costs[0], dp[i-7] + costs[1], dp[i-30] + costs[2])
+                elif 7 <= i < 30:
+                    dp[i] = min(dp[i-1] + costs[0], dp[i-7] + costs[1], costs[2])
                 else:
-                    dp[x] = min(dp[x-1]+costs[0],costs[1],costs[2])
-                    
+                    dp[i] = min(dp[i-1] + costs[0], costs[1], costs[2])
+                               
             else:
-                dp[x] = dp[x-1]
-                
-        return dp[max(days)]
+                dp[i] = dp[i-1]
         
+        return dp[-1]
+    
+    
+    
+# Time: O(max(days))
+# Space: O(max(days))
