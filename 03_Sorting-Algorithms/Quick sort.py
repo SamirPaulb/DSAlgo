e39fe0f3181eb49a9f615e3aaa2ee906dafbf560
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-07-03 00:58:45 +0530

    Update Quick sort.py

--- 03_Sorting-Algorithms/Quick sort.py
+++ 03_Sorting-Algorithms/Quick sort.py
@@ -1,33 +1,33 @@
 # Quick Sort Algorithm
-# Learn theory from Abul Bari Sir (YouTube)
+# Learn theory: https://youtu.be/7h1s2SojIRw
 
-a = [10, 7, 8, 9, 1, 5]
+def partition(arr, l, r):
+    p = l
+    while l <= r:
+        while arr[l] <= arr[p] and l < r:
+            l += 1
+        while arr[r] >= arr[p] and r > l:
+            r -= 1
+        if l < r: arr[l], arr[r] = arr[r], arr[l]
+        l += 1
+        r -= 1
+    arr[p], arr[r] = arr[r], arr[p]
+    return r
 
-def pivot_place(a, first, last):
-    pivot = a[first]
-    left = first + 1
-    right = last
-    while True:
-        while left <= right and a[left] <= pivot:
-            left += 1
-        while left <= right and a[right] >= pivot:
-            right -= 1
-        if right < left:
-            break
-        else:
-            a[left], a[right] = a[right], a[left]
-    a[first], a[right] = a[right], a[first]
-    return right
+def quicksort(arr, l, r):
+    if l >= r: return arr
+    j = partition(arr, l, r)
+    quicksort(arr, l, j)
+    quicksort(arr, j+1, r)
 
-def quicksort(a, first, last):
-    if first < last:
-        p = pivot_place(a, first, last)
-        quicksort(a, first, p-1)
-        quicksort(a, p+1, last)
-
-
-quicksort(a, 0, len(a)-1)
-print(a)
+arr = [10, 16, 8, 12, 15, 3, 9, 5]
+quicksort(arr, 0, len(arr)-1)
+print(arr)
 
 
+# Average Time: O(N log(N))
+# WORST Case TIME Complexity: O(N^2)   
 
+# when the array is already sorted we need to sort it again.
+# when the array is already sorted we need to assign pivot to every element and check 
+# the whole array so Worst case time is O(N^2)
