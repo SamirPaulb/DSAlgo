053c7a642229dc6082a922dc2bd07ce4a4e6afc7
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-07-04 16:17:42 +0530

    Update 04. Largest BST in Binary Tree.py

--- 06_Binary-Trees/04. Largest BST in Binary Tree.py
+++ 06_Binary-Trees/04. Largest BST in Binary Tree.py
@@ -1,2 +1,21 @@
 # https://practice.geeksforgeeks.org/problems/largest-bst/1#
 
+class Solution:
+    # Return the size of the largest sub-tree which is also a BST
+    def largestBst(self, root):
+        def solve(root):
+            if not root: 
+                return True, float('inf'), float('-inf'), 0
+            if not root.left and not root.right:
+                return True, root.data, root.data, 1
+            lBST, lMin, lMax, lRes = solve(root.left)
+            rBST, rMin, rMax, rRes = solve(root.right)
+            if lBST and rBST and lMax < root.data < rMin:
+                return True, min(lMin, root.data), max(rMax, root.data), 1 + lRes + rRes
+            else:
+                return False, 0, 0, max(lRes, rRes)
+        
+        return solve(root)[3]
+      
+# Time: O(N)
+# Auxiliary Space: O(N)
