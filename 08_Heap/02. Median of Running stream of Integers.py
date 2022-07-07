75158e125b1029ce9867400ce4c5355f16653a37
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-07-07 15:54:36 +0530

    Update 02. Median of Running stream of Integers.py

--- 08_Heap/02. Median of Running stream of Integers.py
+++ 08_Heap/02. Median of Running stream of Integers.py
@@ -3,6 +3,32 @@
 # https://www.interviewbit.com/blog/find-median-in-a-stream/
 
 
+class Solution:
+    def __init__(self):
+        self.min_heap = []
+        self.max_heap = []
+        
+    def balanceHeaps(self):
+        #Balance the two heaps size , such that difference is not more than one.
+        if self.min_heap and self.max_heap and - self.min_heap[0] > self.max_heap[0]:
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+        if len(self.min_heap) > len(self.max_heap) + 1:
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
 
+    def getMedian(self):
+        # return the median of the data received till now.
+        if len(self.min_heap) > len(self.max_heap):
+            return -self.min_heap[0]
+        if len(self.max_heap) > len(self.min_heap):
+            return self.max_heap[0]
+        else:
+            return (-self.min_heap[0] + self.max_heap[0]) // 2
+
+    def insertHeaps(self,x):
+        heapq.heappush(self.min_heap, -x)
+        self.balanceHeaps()
+            
 
 
