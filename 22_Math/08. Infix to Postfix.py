206dc3318e7d82eae3edf3a4abdbe3341f666949
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-07-08 15:22:40 +0530

    Create 08. Infix to Postfix.py

---
+++ 22_Math/08. Infix to Postfix.py
@@ -0,0 +1,29 @@
+# https://practice.geeksforgeeks.org/problems/infix-to-postfix-1587115620/1#
+
+class Solution:
+    def InfixtoPostfix(self, exp):
+        res = ""
+        stack = []
+        priorityOrder = {'+':0, '-':0, '*':1, '/':1, '^':2}
+        
+        for c in exp:
+            if c not in '+-*/^()':
+                res += c
+            elif c == "(":
+                stack.append(c)
+            elif c == ")":
+                op = stack.pop()
+                while stack and op != "(":
+                    res += op
+                    op = stack.pop()
+            else:
+                while stack and stack[-1] in priorityOrder and priorityOrder[c] <= priorityOrder[stack[-1]]:
+                    res += stack.pop()
+                stack.append(c)
+                    
+        while stack:
+            res += stack.pop()
+            
+        return res
+        
+        
