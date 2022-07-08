0ac3be428631349c83cac4ab77cbabdecdacecfb
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-07-08 10:31:43 +0530

    Create 07. Transform to Reverse Polish Notation - RPN - Postfix.py

---
+++ 22_Math/07. Transform to Reverse Polish Notation - RPN - Postfix.py
@@ -0,0 +1,36 @@
+# https://www.spoj.com/problems/ONP/
+
+n = int(input(""))  # number of input string
+for i in range(n):
+    s = input('')   # input Algebraic Expression with Brackets
+    stack = []
+    for i in s:
+        if i == ')':
+            tmp = ""
+            while stack:
+                t = stack.pop()
+                if t == "(":
+                    break 
+                elif t in "+-*/^":
+                    tmp += t 
+                else:
+                    tmp = t + tmp
+            stack.append(tmp)
+        else:
+            stack.append(i)
+    print("".join(stack))
+    
+
+    
+'''
+Input:
+3
+(a+(b*c))
+((a+b)*(z+x))
+((a+t)*((b+(a+c))^(c+d)))
+
+Output:
+abc*+
+ab+zx+*
+at+bac++cd+^*
+'''
