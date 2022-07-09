22c219a96d87796eb73f3be4cb68d1f236a81241
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-07-09 16:11:38 +0530

    Update 04. Number of Distinct Substrings in a String.py

--- 10_Trie/04. Number of Distinct Substrings in a String.py
+++ 10_Trie/04. Number of Distinct Substrings in a String.py
@@ -1,5 +1,23 @@
 # https://www.codingninjas.com/codestudio/problems/count-distinct-substrings_985292
 # https://youtu.be/RV0QeTyHZxo
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+
+def countDistinctSubstrings(s):
+    root = TrieNode()
+    res = 0
+    for i in range(len(s)):
+        cur = root
+        for j in range(i, len(s)):
+            if s[j] not in cur.children:
+                cur.children[s[j]] = TrieNode()
+                res += 1
+            cur = cur.children[s[j]]
+    return res + 1
+
+'''
 class TrieNode:
     def __init__(self):
         self.children = {}
@@ -21,7 +39,7 @@ class Trie:
 def countDistinctSubstrings(s):
     trie = Trie()
     return trie.solve(s)
-
+'''
 
 # Time: O(N^2)
 # Space: It is hard to predict spcace taken tries. It depends on the distinct elements of s. But as we are using only necessary keys in trie hashmap not all 26 keys so at max space can be N^2
