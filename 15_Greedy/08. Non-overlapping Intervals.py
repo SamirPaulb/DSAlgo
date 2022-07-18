776390a11e3f2838985c8ee87ebf9afd7f423337
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-07-18 19:22:06 +0530

    Create 08. Non-overlapping Intervals.py

---
+++ 15_Greedy/08. Non-overlapping Intervals.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/non-overlapping-intervals/
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        res = 0
+        preEnd = intervals[0][1]
+        # print(intervals)
+        for cur in intervals[1:]:
+            curStart = cur[0]
+            curEnd = cur[1]
+            if curStart < preEnd:
+                preEnd = min(preEnd, curEnd)
+                res += 1
+            else:
+                preEnd = curEnd
+        
+        return res
+      
+      
+# Time: O(N)
+# Space: O(1)
