4c3f86aa136ceb3bdf3d2ca6a32b5820106dd913
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-07-20 22:00:45 +0530

    Create 03. Maximum sum Rectangle in Matrix.py

---
+++ 02_Dynamic-Programming/08. Kadanes Algorithm/03. Maximum sum Rectangle in Matrix.py
@@ -0,0 +1,28 @@
+# https://practice.geeksforgeeks.org/problems/maximum-sum-rectangle2948/1
+
+class Solution:
+    def maximumSumRectangle(self,R,C,M):
+        
+        def kadanes(arr):
+            cs = 0
+            ms = arr[0]
+            for a in arr:
+                cs += a
+                cs = max(cs, a)
+                ms = max(ms, cs)
+            return ms
+        
+        res = kadanes(M[0])
+        arr = [0] * C
+        for i in range(R):
+            arr = [0] * C
+            for r in range(i, R):
+                for c in range(C):
+                    arr[c] += M[r][c]
+                res = max(res, kadanes(arr))
+        
+        return res
+        
+        
+# Time Complexity: O(R * R * C)
+# Auxiliary Space: O(C)
