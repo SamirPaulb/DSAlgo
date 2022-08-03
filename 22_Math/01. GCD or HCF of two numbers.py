9de622314c63739bfeeaa97ff0def4430889c359
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-08-03 10:44:48 +0530

    Update 01. GCD or HCF of two numbers.py

--- 22_Math/01. GCD or HCF of two numbers.py
+++ 22_Math/01. GCD or HCF of two numbers.py
@@ -8,6 +8,27 @@ of two numbers is the largest number that divides both of them.
 
 # --------------------------------------------------------------------------------------------------
 
+def gcd(a, b):
+  # Find minimum of a and b
+  result = min(a, b)
+   
+  while result:
+    if a % result == 0 and b % result == 0:
+      break
+    result -= 1
+   
+  # Return the gcd of a and b
+  return result
+ 
+# Driver Code
+a = 98
+b = 56
+ 
+print(f"GCD of {a} and {b} is {gcd(a, b)}")
+
+
+# --------------------------------------------------------------------------------------------------
+
 # Recursive function to return gcd of a and b
 def gcd(a,b):
      
