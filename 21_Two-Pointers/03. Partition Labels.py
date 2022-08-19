a16c5908792c59e9c2d62bd4c26d154f22bd1c4a
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-08-19 23:07:08 +0530

    Update 03. Partition Labels.py

--- 21_Two-Pointers/03. Partition Labels.py
+++ 21_Two-Pointers/03. Partition Labels.py
@@ -2,28 +2,18 @@
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        countDict = {}
-        for i in s:
-            if i not in countDict:
-                countDict[i] = 1
-            else:
-                countDict[i] += 1
-                
+        
+        lastIndex = {ch:i for i, ch in enumerate(s)}
         res = []
-        i = 0
-        while i < len(s):
-            targetSum = 0
-            track = set()
-            j = i
-            while j < len(s):
-                if s[j] not in track:
-                    track.add(s[j])
-                    targetSum += countDict[s[j]]
-                if j-i+1 == targetSum:
-                    res.append(j-i+1)
-                    break
-                j += 1
-            i = j + 1
+        left = -1
+        right = 0
+        
+        for i, ch in enumerate(s):
+            right = max(right, lastIndex[ch])
+            if i == right:
+                res.append(right - left)
+                right = i
+                left = i
         
         return res
     
