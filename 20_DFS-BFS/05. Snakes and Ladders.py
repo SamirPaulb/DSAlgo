27f36b2d26c40103993634d3fe89979144b9ceca
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-08-23 22:05:13 +0530

    Create 05. Snakes and Ladders.py

---
+++ 20_DFS-BFS/05. Snakes and Ladders.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/snakes-and-ladders/
+
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        n = len(board)
+        def label_to_position(label):
+            r, c = divmod(label-1, n)
+            if r % 2 == 0:
+                return n-1-r, c
+            else:
+                return n-1-r, n-1-c
+            
+        seen = set()
+        queue = collections.deque()
+        queue.append((1, 0))
+        while queue:
+            label, step = queue.popleft()
+            r, c = label_to_position(label)
+            if board[r][c] != -1:
+                label = board[r][c]
+            if label == n*n:
+                return step
+            for x in range(1, 7):
+                new_label = label + x
+                if new_label <= n*n and new_label not in seen:
+                    seen.add(new_label)
+                    queue.append((new_label, step+1))
+        return -1
+
+      
+      # Time: O(n^2)
+      # Space: O(n^2)
