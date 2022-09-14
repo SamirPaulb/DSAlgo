8c6ddaa4120b211a523136afe62a1078a93d49bc
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-09-14 14:12:20 +0530

    Update 07. Minimum distance between two numbers.py

--- 18_Array/07. Minimum distance between two numbers.py
+++ 18_Array/07. Minimum distance between two numbers.py
@@ -1,5 +1,6 @@
 # https://practice.geeksforgeeks.org/problems/minimum-distance-between-two-numbers/1
 
+'''
 class Solution:
     def minDist(self, arr, n, x, y):
         res = 2**31
@@ -15,7 +16,19 @@ class Solution:
                 j += 1
 
         return res if res != 2**31 else -1
-      
+'''
+
+class Solution:
+    def minDist(self, arr, n, x, y):
+        res = 2**31
+        ix = iy = -1
+        
+        for i in range(n):
+            if arr[i] == x: ix = i
+            if arr[i] == y: iy = i
+            if ix != -1 and iy != -1: res = min(res, abs(ix - iy))
+            
+        return res if res != 2**31 else -1
       
       
 '''
