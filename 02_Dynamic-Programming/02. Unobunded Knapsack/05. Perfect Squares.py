6e607d110943db75ebf81b760ea2c01e7db0686e
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-10-04 19:37:23 +0530

    Create 05. Perfect Squares.py

---
+++ 02_Dynamic-Programming/02. Unobunded Knapsack/05. Perfect Squares.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/perfect-squares/
+
+'''
+# Coin Change
+class Solution:
+    def numSquares(self, n: int) -> int:
+        row = int(math.sqrt(n)) + 1
+        dp = [[2**31]*(n+1) for i in range(row)]
+        for i in range(row):
+            dp[i][0] = 0
+        for i in range(1, row):
+            for j in range(1, n+1):
+                if i**2 <= j:
+                    dp[i][j] = min(dp[i-1][j], 1 + dp[i][j - i**2])
+                else:
+                    dp[i][j] = dp[i-1][j]
+        # print(dp)
+        return dp[-1][-1]
+        
+# Time: O(N * N^1/2)
+# Space: O(N * N^1/2)
+'''
+
+# 1D DP
+# https://youtu.be/HLZLwjzIVGo
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [n] * (n+1)
+        dp[0] = 0
+        for target in range(1, n+1):
+            for s in range(target):
+                sq = s * s
+                if sq > target: break
+                dp[target] = min(dp[target], 1 + dp[target - sq])
+        
+        return dp[n]
+    
+# Time: O(N * N^1/2)
+# Space: O(N)
