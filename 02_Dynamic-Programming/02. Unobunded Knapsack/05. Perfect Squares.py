5b394eb5ec9a997dd646779b681462bef305e70e
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-10-04 19:45:50 +0530

    Update 05. Perfect Squares.py

--- 02_Dynamic-Programming/02. Unobunded Knapsack/05. Perfect Squares.py
+++ 02_Dynamic-Programming/02. Unobunded Knapsack/05. Perfect Squares.py
@@ -2,6 +2,7 @@
 
 '''
 # Coin Change
+
 class Solution:
     def numSquares(self, n: int) -> int:
         row = int(math.sqrt(n)) + 1
@@ -21,17 +22,20 @@ class Solution:
 # Space: O(N * N^1/2)
 '''
 
+
 # 1D DP
 # https://youtu.be/HLZLwjzIVGo
+
 class Solution:
     def numSquares(self, n: int) -> int:
         dp = [n] * (n+1)
         dp[0] = 0
-        for target in range(1, n+1):
-            for s in range(target):
+        for i in range(1, n+1):
+            for s in range(i):
                 sq = s * s
-                if sq > target: break
-                dp[target] = min(dp[target], 1 + dp[target - sq])
+                if sq > i: break
+                if 1 + dp[i - sq] < dp[i]:
+                    dp[i] = 1 + dp[i - sq]
         
         return dp[n]
     
