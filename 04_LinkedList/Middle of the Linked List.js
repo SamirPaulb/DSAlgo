49d4be2a6af59a74251e41ca5af7b7e8124a7c4c
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-10-22 21:23:51 +0530

    Update and rename Middle of the Linked List.js to Middle of the Linked List.py

--- 04_LinkedList/Middle of the Linked List.js
+++
@@ -1,9 +0,0 @@
-var middleNode = function(head) {
-    let slow = head;
-    let fast = head;
-    while(fast!=null && fast.next!=null){
-        slow=slow.next;
-        fast=fast.next.next;
-    }
-    return slow;
-};
