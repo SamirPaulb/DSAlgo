038d0de54b06d869381bb9fcd0f632edc59c5c3f
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-11-02 18:32:56 +0530

    Update 08. Guess Number Higher or Lower II.py

--- 02_Dynamic-Programming/09. DP on Grid/08. Guess Number Higher or Lower II.py
+++ 02_Dynamic-Programming/09. DP on Grid/08. Guess Number Higher or Lower II.py
@@ -17,3 +17,24 @@ class Solution:
         # print(dp)
         return solve(1, n)
       
+        
+class Solution:
+    def getMoneyAmount(self, n: int) -> int:
+        dp = [[2**31]*(n+1) for _ in range(n+1)]
+        
+        for i in range(n, 0, -1):
+            for j in range(i, n+1):
+                if i == j:
+                    dp[i][j] = 0
+                if j - i == 1: 
+                    dp[i][j] = i
+                else:
+                    for k in range(i, j):
+                        dp[i][j] = min(dp[i][j], k + max(dp[i][k-1], dp[k+1][j]))
+        # print(dp)
+        return dp[1][-1]
+    
+    
+    
+# Time: O(N * N * N)
+# Space: O(N * N)
