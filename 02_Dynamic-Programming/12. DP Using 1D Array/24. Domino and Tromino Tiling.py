fa3ab7194bfb5933202b81dacae348468475b0ba
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-11-08 01:10:18 +0530

    Update 24. Domino and Tromino Tiling.py

--- 02_Dynamic-Programming/12. DP Using 1D Array/24. Domino and Tromino Tiling.py
+++ 02_Dynamic-Programming/12. DP Using 1D Array/24. Domino and Tromino Tiling.py
@@ -1,6 +1,27 @@
 # https://leetcode.com/problems/domino-and-tromino-tiling/
 # https://youtu.be/7cijrfUkQzc
 
+'''
+Purely based on observation and by intuition you can arrive at the follwing formula->
+f(n)=f(n-1)+f(n-2)+2*(f(n-3)+f(n-4)+......+f(n-n))      ------(1)
+
+if we remove 1 width  and 2 length tile vertically theres 1 way
+if we remove 1 horizontal 2 width and 1 length tile below will also be same width tile so only 1 way
+if we remove 3,by obervation we get 2 ways    
+
+similarly,
+f(n-1) = f(n-2) + f(n-3) + 2*(f(n-4) + ....)   
+f(n-2) = f(n-1) - f(n-3) - 2*(f(n-4) + f(n-5) + ....)   ------(2)
+putting value of f(n-2) to equation (1) we get,
+
+f(n) = 2*f(n-1) + f(n-3)
+
+                         __     and         __
+                                                                       |  __|          |__ |   
+similarly 2 for rest 
+'''
+
+
 class Solution:
     def numTilings(self, n: int) -> int:
         MOD = 10**9 + 7
