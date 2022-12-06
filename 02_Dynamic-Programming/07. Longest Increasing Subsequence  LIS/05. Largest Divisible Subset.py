d9218b324f9769b8ff830b8eaa82aea7a5370db6
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-12-06 18:10:08 +0530

    Create 05. Largest Divisible Subset.py

---
+++ 02_Dynamic-Programming/07. Longest Increasing Subsequence  LIS/05. Largest Divisible Subset.py
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/largest-divisible-subset/description/
+  
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        numDict = {num:[num] for num in nums}
+
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] % nums[j] == 0 and len(numDict[nums[i]]) < len(numDict[nums[j]]) + 1:
+                    numDict[nums[i]] = numDict[nums[j]] + [nums[i]]
+        
+        res = []
+        for num in numDict:
+            if len(numDict[num]) > len(res):
+                res = numDict[num]
+
+        return res
+      
+      
+# Time: O(N^2)
+# Space: O(N^2)
