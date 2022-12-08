1baa746178ffebb1987d23bce3b29c2bc0ecf89f
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2022-12-08 21:36:09 +0530

    Create 21. 132 Pattern.py

---
+++ 13_Stack/21. 132 Pattern.py
@@ -0,0 +1,18 @@
+# https://youtu.be/q5ANAl8Z458
+
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        curMin = nums[0]
+        stack = []  # stack contains [nums[i], nums[j]] and n = nums[k] 
+        
+        for n in nums[1:]:
+            while stack and n >= stack[-1][1]:
+                stack.pop()
+            if stack and n > stack[-1][0]: return True
+            stack.append([curMin, n])
+            curMin = min(curMin, n)
+        
+        return False
+      
+      
+      
