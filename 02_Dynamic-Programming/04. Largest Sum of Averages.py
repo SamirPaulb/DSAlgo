543397c7b32871a266b05c784c94511229ca62ce
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-12-20 13:56:20 +0530

    Create 04. Largest Sum of Averages.py

---
+++ 02_Dynamic-Programming/04. Largest Sum of Averages.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/largest-sum-of-averages/
+
+class Solution:
+    def largestSumOfAverages(self, nums: List[int], k: int) -> float:
+        dp = [[-1] * (k+1) for i in range(len(nums))]
+        
+        def dfs(i, k):      # Time: O(N * K)
+            if i >= len(nums) or k < 0: return 0
+            if k == 0: return -2**31
+            if dp[i][k] != -1: return dp[i][k]
+            ans = 0
+            s = 0
+            for j in range(i, len(nums)):   # Time: O(N)
+                s += nums[j]
+                ans = max(ans, s/(j-i+1) + dfs(j+1, k-1))
+            dp[i][k] = ans
+            return ans
+        
+        return dfs(0, k)
+    
+    
+# Time: O(K * N * N)
+# Space: O(K * N * N)
