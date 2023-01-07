b42890d1fb23fca47268b8acd9390beb36b04440
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-01-07 11:41:27 +0530

    Update 31. Count Numbers with Unique Digits.py

--- 22_Math/31. Count Numbers with Unique Digits.py
+++ 22_Math/31. Count Numbers with Unique Digits.py
@@ -32,4 +32,18 @@ class Solution:
             
         return ans
     
-# Time: O(1)
+
+# Dynamic Programming
+
+class Solution:
+    def countNumbersWithUniqueDigits(self, n):
+        dp = [1, 10]
+        for i in range(2, n+1):
+            tmp = 81
+            for j in range(1, i-1):
+                tmp *= (9 - j)
+            ans = dp[i-1] + tmp
+            dp.append(ans)
+        
+        return dp[n]
+                
