a6cf4ff1fcf050e1b018f6e728050356228d8010
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-09 18:45:48 +0530

    Update 08. Word Search II.py

--- 10_Trie/08. Word Search II.py
+++ 10_Trie/08. Word Search II.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/word-search-ii/
 # https://youtu.be/asbcE9mZz_U
+
 '''
 In Brute Force DFS time complexity: O(k * m * n * 4^(m*n)) where k = len(words)
 But we can remove the k if we store all the words into a TRIE.
@@ -8,7 +9,7 @@ But we can remove the k if we store all the words into a TRIE.
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.endOfWord = False
+        self.isWord = False
     
     def addWord(self, word):
         cur = self
@@ -16,40 +17,38 @@ class TrieNode:
             if c not in cur.children:
                 cur.children[c] = TrieNode()
             cur = cur.children[c]
-        cur.endOfWord = True
-    
+        cur.isWord = True
+        
+
 class Solution:
     def findWords(self, board, words):
         root = TrieNode()
-        for word in words:
-            root.addWord(word)
-        
         row = len(board); col = len(board[0])
         res = set()
-        
+        for word in words:
+            root.addWord(word)
+                
         def dfs(r, c, node, word):
-            if (r < 0 or c < 0 or r >= row or c >= col or 
-                board[r][c] not in node.children or board[r][c] == '#'):
+            if not 0<=r<row or not 0<=c<col or board[r][c] == "#" or board[r][c] not in node.children:
                 return
-            
             word += board[r][c]
             node = node.children[board[r][c]]
-            if node.endOfWord:
+            if node.isWord: 
                 res.add(word)
-                node.endOfWord = False
-            tmp = board[r][c]
-            board[r][c] = '#'
-            dfs(r-1, c, node, word)
-            dfs(r+1, c, node, word)
-            dfs(r, c-1, node, word)
-            dfs(r, c+1, node, word)
-            board[r][c] = tmp
-            
+                node.isWord = False
+                
+            cur = board[r][c]
+            board[r][c] = "#"
+            for dx, dy in ((1,0),(-1,0),(0,1),(0,-1)):
+                dfs(r+dx, c+dy, node, word)
+            board[r][c] = cur
+        
         for r in range(row):
             for c in range(col):
                 dfs(r, c, root, "")
         
         return res
+    
     
 # Time: O(m * n * 4^(m*n))
 # Space: O(m*n + k)
