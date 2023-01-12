eb0c5960c1d2c16378734ecf8a2e5c67a3a5db58
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-01-12 19:55:05 +0530

    Create 09. Koko Eating Bananas.py

---
+++ 11_Binary-Search/09. Koko Eating Bananas.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/koko-eating-bananas/
+# https://github.com/SamirPaul1/DSAlgo/blob/main/11_Binary-Search/01.%20Allocate%20Minimum%20Number%20Of%20Pages.py
+
+class Solution:
+    def minEatingSpeed(self, piles, h):
+        def isValid(speed):
+            time = 0
+            for num in piles:
+                time += math.ceil(num/speed)
+            return time <= h
+        
+        l, r, res = 1, max(piles), max(piles)
+        while l <= r:
+            mid = l + (r-l)//2
+            if isValid(mid):
+                res = min(res, mid)
+                r = mid - 1
+            else:
+                l = mid + 1
+        
+        return res
+    
+    
+# Time: O(N * log(M))      where M = max(piles)
+# Space: O(1)
