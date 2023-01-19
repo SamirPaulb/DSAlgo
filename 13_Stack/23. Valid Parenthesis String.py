7b071ef609e68d52efb92252385993a53bc710e0
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-01-20 01:17:42 +0530

    Create 23. Valid Parenthesis String.py

---
+++ 13_Stack/23. Valid Parenthesis String.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/valid-parenthesis-string/
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        openStack = []
+        starStack = []
+        for i, ch in enumerate(s):
+            if ch == ')':
+                if openStack:
+                    openStack.pop()
+                elif starStack:
+                    starStack.pop()
+                else: 
+                    return False
+            elif ch == '(':
+                openStack.append(i)
+            else:
+                starStack.append(i)
+        
+        while openStack and starStack:
+            if openStack[-1] > starStack[-1]:
+                return False
+            openStack.pop()
+            starStack.pop()
+        
+        return len(openStack) == 0
+      
+      
+# Time: O(N)
+# Space: O(N)
