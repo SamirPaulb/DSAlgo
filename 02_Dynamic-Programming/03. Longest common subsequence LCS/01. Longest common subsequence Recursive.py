222baeec347911c1f311d072d7821d822670dcc2
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-03-02 18:46:15 +0530

    Update 01. Longest common subsequence Recursive.py

--- 02_Dynamic-Programming/03. Longest common subsequence LCS/01. Longest common subsequence Recursive.py
+++ 02_Dynamic-Programming/03. Longest common subsequence LCS/01. Longest common subsequence Recursive.py
@@ -28,3 +28,23 @@ But this approach will give you (Time Limit Exceeded) error for
 s = Solution()
 print s.longestCommonSubsequence("ylqpejqbalahwr", "yrkzavgdmdgtqpg")
 '''
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        dp = [[0]*(n+1) for i in range(m+1)]
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    
+        return dp[-1][-1]
+    
+    
+# Time: O(n * m)
+# Space: O(n * m)
