bead43bb0105b29461adcacf478c4aa7c0707cc0
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-03-07 11:14:28 +0530

    Update 06. Burst Balloons.py

--- 02_Dynamic-Programming/04. Matrix Chain Multiplication/06. Burst Balloons.py
+++ 02_Dynamic-Programming/04. Matrix Chain Multiplication/06. Burst Balloons.py
@@ -1,22 +1,23 @@
 # https://leetcode.com/problems/burst-balloons/
-# https://youtu.be/VFskby7lUbw
+# https://youtu.be/uG_MtaCJIrM
 
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
         nums = [1] + nums + [1]
-        dp = [[-1]*len(nums) for _ in range(len(nums))]
+        dp = [[-1]*302 for _ in range(302)]
         
-        def dfs(l, r):
+        def solve(l, r):
             if l > r: return 0
             if dp[l][r] != -1: return dp[l][r]
-            dp[l][r] = 0
-            for i in range(l, r+1):
-                coins = nums[l-1] * nums[i] * nums[r+1]  # Burst i'th balloon last
-                coins += dfs(l, i-1) + dfs(i+1, r)
-                dp[l][r] = max(dp[l][r], coins)
+            ans = 0
+            for k in range(l, r+1):
+                tmp = nums[l-1] * nums[k] * nums[r+1]
+                tmp += solve(l, k-1) + solve(k+1, r)
+                ans = max(ans, tmp)
+            dp[l][r] = ans
             return dp[l][r]
         
-        return dfs(1, len(nums)-2)
+        return solve(1, len(nums)-2)
     
     
 # Time: O(N^3)
