4cb98cbebc636a6c4532640edf0c5053d45e37ea
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-03-09 16:59:00 +0530

    Update 03. Number of Longest Increasing Subsequence.py

--- 02_Dynamic-Programming/07. Longest Increasing Subsequence  LIS/03. Number of Longest Increasing Subsequence.py
+++ 02_Dynamic-Programming/07. Longest Increasing Subsequence  LIS/03. Number of Longest Increasing Subsequence.py
@@ -1,31 +1,31 @@
 # https://leetcode.com/problems/number-of-longest-increasing-subsequence/
 # https://youtu.be/Tuc-rjJbsXU
 
-
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [1] * n
-        count = [1] * n
+        dp = [1]*len(nums)  # length of LIS
+        count = [1]*len(nums)   # count of LIS
         
-        maxi = 0
-        for i in range(n):
-            for j in range(i):
-                if nums[i] > nums[j] and dp[j] + 1 > dp[i]:
-                    dp[i] = dp[j] + 1
-                    count[i] = count[j]     # inherit count
-                elif nums[i] > nums[j] and dp[j] + 1 == dp[i]:
-                    count[i] += count[j]    # increase the count
-            maxi = max(maxi, dp[i])
-            
+        for r in range(len(nums)):
+            for l in range(r):
+                if nums[l] < nums[r] and dp[l] + 1 > dp[r]:
+                    dp[r] = dp[l] + 1
+                    count[r] = count[l]  # inherit count
+                elif nums[l] < nums[r] and dp[l] + 1 == dp[r]:
+                    count[r] += count[l] # increase the count
+        
+        maxi = max(dp)
         res = 0
-        for i in range(n):
+        for i in range(len(nums)):
             if dp[i] == maxi:
                 res += count[i]
-        
+        # print(nums)    # [1, 5, 4, 3, 2, 6, 7,10, 8, 9]
+        # print(dp)      # [1, 2, 2, 2, 2, 3, 4, 5, 5, 6]
+        # print(count)   # [1, 1, 1, 1, 1, 4, 4, 4, 4, 4]
         return res
+    
+    
     
     
 # Time: O(N^2)
 # Space: O(N)
-
