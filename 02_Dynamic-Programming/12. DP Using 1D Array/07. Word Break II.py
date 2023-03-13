2a6d11cccdce382135dd4b9d712359d86b3e48f0
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-03-14 01:04:35 +0530

    Update 07. Word Break II.py

--- 02_Dynamic-Programming/12. DP Using 1D Array/07. Word Break II.py
+++ 02_Dynamic-Programming/12. DP Using 1D Array/07. Word Break II.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/word-break-ii/
 
+# DP + DFS
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         dic = {i : [] for i in range(len(s))}
@@ -24,3 +25,23 @@ class Solution:
         
         solve(0, "")
         return res
+
+    
+    # DP
+    class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        n = len(s)
+        dp = [[""]] * (n+1)
+        for i in range(1, n+1):
+            arr = []
+            for word in wordDict:
+                if i - len(word) >= 0 and s[i-len(word):i] == word:
+                    if i-len(word) == 0:
+                        arr.append(word)
+                    elif dp[i-len(word)] != [""]:
+                        for st in dp[i-len(word)]:
+                            arr.append(st + " " + word)
+                            
+            dp[i] = arr
+        # print(dp)
+        return dp[-1]
