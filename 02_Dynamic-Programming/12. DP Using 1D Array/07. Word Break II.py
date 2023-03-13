33dbd621882867934d7700e734153b722b6db84a
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-03-14 01:36:31 +0530

    Update 07. Word Break II.py

--- 02_Dynamic-Programming/12. DP Using 1D Array/07. Word Break II.py
+++ 02_Dynamic-Programming/12. DP Using 1D Array/07. Word Break II.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/word-break-ii/
 
-# DP + DFS
+# Method-1:  DP + DFS
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         dic = {i : [] for i in range(len(s))}
@@ -30,8 +31,8 @@ class Solution:
     
     
     
-    
-# DP
+# Method-2: DP
+
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         n = len(s)
@@ -49,3 +50,34 @@ class Solution:
             
         # print(dp)
         return dp[-1]
+
+    
+    
+    
+    
+# Method-3: DP + DFS (Similar to Method-1)
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        n = len(s)
+        dp = [[] for _ in range(n+1)]
+        for i in range(1, n+1):
+            for word in wordDict:
+                if i - len(word) >= 0 and s[i-len(word):i] == word:
+                    dp[i].append(word)
+        
+        res = []
+        if not dp[-1]: return res
+        # print(dp)
+        def solve(i, path):
+            if i <= 0: 
+                res.append(path)
+                return
+            for st in dp[i]:
+                if not path:
+                    solve(i-len(st), st)
+                else:
+                    solve(i-len(st), st + " " + path)
+        
+        solve(n, "")
+        return res
