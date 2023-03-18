8e778871131b5ecf87c25a6bcc6a784d849268b2
Priangshu Gupta <93185814+sbkv1@users.noreply.github.com>
2023-03-19 00:19:39 +0530

    Update 06. Design Browser History.py

--- 04_LinkedList/06. Design Browser History.py
+++ 04_LinkedList/06. Design Browser History.py
@@ -1,13 +1,15 @@
 # https://leetcode.com/problems/design-browser-history/ 
 
+
+# Method 1 --------- Doubly LinkedList ---------
+
 class ListNode:
     
     def __init__(self, x):
         self.val = x
         self.next = None
         self.prev = None
-
-
+        
 class BrowserHistory:
 
     def __init__(self, homepage: str):
@@ -21,19 +23,42 @@ class BrowserHistory:
 
     def back(self, steps: int) -> str:
         while steps and self.head.prev:
-            steps -= 1
             self.head = self.head.prev
+            steps -= 1
         return self.head.val
 
     def forward(self, steps: int) -> str:
         while steps and self.head.next:
-            steps -= 1
             self.head = self.head.next
+            steps -= 1
         return self.head.val
     
 
-# Your BrowserHistory object will be instantiated and called as such:
-# obj = BrowserHistory(homepage)
-# obj.visit(url)
-# param_2 = obj.back(steps)
-# param_3 = obj.forward(steps)
+    
+    
+    
+    
+# Method 2 --------- Array ---------
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.pages = [homepage]
+        self.i = 0
+
+    def visit(self, url: str) -> None:
+        self.pages = self.pages[:self.i+1] + [url]
+        self.i += 1
+
+    def back(self, steps: int) -> str:
+        for i in range(steps):
+            self.i -= 1
+        if self.i < 0: self.i = 0
+        return self.pages[self.i]
+        
+    def forward(self, steps: int) -> str:
+        for i in range(steps):
+            self.i += 1
+        if self.i >= len(self.pages): self.i = len(self.pages)-1
+        return self.pages[self.i]
+    
