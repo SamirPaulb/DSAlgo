8dc8d48847c92dccab13c869252bccce1fd3e7ac
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-03-22 19:55:20 +0530

    Update 25. Arithmetic Slices.py

--- 02_Dynamic-Programming/12. DP Using 1D Array/25. Arithmetic Slices.py
+++ 02_Dynamic-Programming/12. DP Using 1D Array/25. Arithmetic Slices.py
@@ -1,41 +1,34 @@
 # https://leetcode.com/problems/arithmetic-slices/
+# https://leetcode.com/problems/arithmetic-slices/solutions/1455367/Python-Bottom-up-DP-Time-O(N)-Space-O(1)-Clean-and-Concise/
 
-'''# observe the sequence
-In arr assume index (r - l) is max length of arithmetic subarray.
-for length = 3; number of arithmetic subarrays = 1
-for length = 4; number of arithmetic subarrays = 3
-for length = 5; number of arithmetic subarrays = 6
-for length = 6; number of arithmetic subarrays = 10
-for length = 7; number of arithmetic subarrays = 15
-
-nums = [1, 2, 3, 4, 5, 6,  7]
-arr  = [0, 0, 1, 3, 6, 10, 15]
-
-'''
 
 
 class Solution:
-    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+    def numberOfArithmeticSlices(self, nums):
         n = len(nums)
-        if n < 3: return 0
-        if n == 3: return 1
-        arr = [0] * (n+1)
-        arr[3] = 1
-        for i in range(4, n+1):
-            arr[i] = arr[i-1] + (i-3+1)
-        # print(arr)
-        res = 0
-        l = 0; r = 1; diff = nums[r] - nums[l]
-        while r < n:
-            if nums[r] - nums[r-1] != diff:
-                diff = nums[r] - nums[r-1] 
-                res += arr[r-l]
-                l = r-1
-            r += 1
-        res += arr[r-l]
+        dp = [0] * n
+        for i in range(2, n):
+            if nums[i] - nums[i-1] == nums[i-1] - nums[i-2]:
+                dp[i] = dp[i-1] + 1
+        
+        return sum(dp)
+    
+# Time: O(n)
+# Space: O(n)
+    
+    
+
+class Solution:
+    def numberOfArithmeticSlices(self, nums):
+        dp = 0; n = len(nums); res = 0
+        for i in range(2, n):
+            if nums[i] - nums[i-1] == nums[i-1] - nums[i-2]:
+                dp += 1
+                res += dp
+            else:
+                dp = 0
+                
         return res
-      
-      
-      
-# Time: O(N)
-# Space: O(N)
+    
+# Time: O(n)
+# Space: O(1)
