54d766c72256cffe7ef27766a0ce7e9a8f51ca74
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-03-25 01:01:22 +0530

    Update 08. Reorder Routes to Make All Paths Lead to the City Zero.py

--- 07_Graph/08. Reorder Routes to Make All Paths Lead to the City Zero.py
+++ 07_Graph/08. Reorder Routes to Make All Paths Lead to the City Zero.py
@@ -1,1 +1,25 @@
+# https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/
 
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        adj = {i:set() for i in range(n)}
+        directedAdj = {i:set() for i in range(n)}
+        
+        for a, b in connections:
+            adj[a].add(b)
+            adj[b].add(a)
+            directedAdj[a].add(b)
+        
+        visited = {i:False for i in range(n)}
+        q = collections.deque([0])
+        res = 0
+        while q:
+            a = q.popleft()
+            visited[a] = True
+            for b in adj[a]:
+                if not visited[b]:
+                    if b in directedAdj[a]:
+                        res += 1
+                    q.append(b)
+        
+        return res
