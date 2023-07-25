01023f4adac4c2c7668b9ade2cce01e8851676ed
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-07-25 18:13:13 +0530

    Update 03. Single Number II.py

--- 17_Bit-Manipulation/03. Single Number II.py
+++ 17_Bit-Manipulation/03. Single Number II.py
@@ -1,5 +1,26 @@
 # https://leetcode.com/problems/single-number-ii/
-# https://youtu.be/cOFAmaMBVps?t=476
+# Explanation: https://spcdn.pages.dev/single-number-ii.pdf
+
+class Solution:
+    def singleNumber(self, nums):
+        res = 0
+        negCount = 0
+        for i in range(32):
+            s = 0
+            for num in nums:
+                if num < 0: 
+                    negCount += 1
+                if (abs(num) >> i) & 1:
+                    s += 1
+                    s %= 3
+            res |= (s << i)
+
+        return -res if negCount%3 else res
+
+
+
+
+        
 
 class Solution:
     # Optimal best approach
