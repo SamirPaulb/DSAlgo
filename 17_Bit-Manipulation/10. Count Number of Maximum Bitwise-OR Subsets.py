a542645d6def000517e26af3143a777b648cd4f4
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-07-26 07:38:48 +0530

    Update 10. Count Number of Maximum Bitwise-OR Subsets.py

--- 17_Bit-Manipulation/10. Count Number of Maximum Bitwise-OR Subsets.py
+++ 17_Bit-Manipulation/10. Count Number of Maximum Bitwise-OR Subsets.py
@@ -1,2 +1,15 @@
 # https://leetcode.com/problems/count-number-of-maximum-bitwise-or-subsets/
 
+
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        maxOR = 0
+        for i in nums:
+            maxOR |= i
+        
+        def dfs(i, val):
+            if val == maxOR: return 1 << (len(nums)-i)
+            if i == len(nums): return 0
+            return dfs(i+1, val | nums[i]) + dfs(i+1, val)
+        
+        return dfs(0, 0)
