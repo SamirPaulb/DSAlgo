e41b804d7f68f9cae1c00f58a51bde004818ef19
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-07-26 23:17:51 +0530

    Update 16. Apply Bitwise Operations to Make Strings Equal.py

--- 17_Bit-Manipulation/16. Apply Bitwise Operations to Make Strings Equal.py
+++ 17_Bit-Manipulation/16. Apply Bitwise Operations to Make Strings Equal.py
@@ -2,7 +2,7 @@
 
 class Solution:
     def makeStringsEqual(self, s: str, target: str) -> bool:
-        return s == target or (int(s) > 0 and int(target) > 0)
+        return s == target or (int(s, 2) > 0 and int(target, 2) > 0)
         
         
 # Time complexity: O(n)
