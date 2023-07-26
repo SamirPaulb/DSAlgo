3e6ffeebe27a6e0c6677ae0b4f5cb738b0e4fe0d
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-07-26 12:32:42 +0530

    Create 20. Maximize XOR.py

---
+++ 17_Bit-Manipulation/20. Maximize XOR.py
@@ -0,0 +1,21 @@
+# https://practice.geeksforgeeks.org/problems/maximize-xor0829/1
+'''
+Step 1:- Just find out the position of unset bit in number.
+Step 2:- Suppose, if unset bit at i'th position then add (2^i)/2 in result variable
+         /2 as that bit will not be consider and all permutation of right-side bits will 
+         be lesser.
+Step 3:- Right shift number n one time
+Step 4:- continue till num become zero(0)
+'''
+
+
+class Solution:
+	def maximize_xor_count(self, n):
+		res = 0
+		i = 1
+		while n:
+		    if n&1 == 0:
+		        res += (2**i)//2
+		    i += 1
+            n = n >> 1
+        return res
