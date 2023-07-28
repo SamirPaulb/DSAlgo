340a38a973dc1ed7ab5456031651b70a9f1de374
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-07-28 20:08:35 +0530

    Update 07. Top K Frequent Elements.py

--- 19_HashMap/07. Top K Frequent Elements.py
+++ 19_HashMap/07. Top K Frequent Elements.py
@@ -4,26 +4,27 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         # Bucket Sort
-        countDict = {}
-        for i in  nums:
-            if i not in countDict:
-                countDict[i] = 1
-            else:
-                countDict[i] += 1
-                
-        n = max(list(countDict.values()))
-        dic = {i:[] for i in range(1, n+1)}
-        for i in countDict:
-            dic[countDict[i]].append(i)
+        num_cnt = collections.Counter()
+        for num in nums:
+            num_cnt[num] += 1
+        
+        cnt_num = {}
+        for num in num_cnt:
+            cnt = num_cnt[num]
+            if cnt not in cnt_num: 
+                cnt_num[cnt] = [num]
+            else: 
+                cnt_num[cnt].append(num)
         
         res = []
-        for i in range(n, 0, -1):
-            if len(dic[i]) > 0:
-                res += dic[i]
-            if len(res) >= k: 
-                break
-                
+        n = max(num_cnt.values())
+        for cnt in range(n, 0, -1):
+            if cnt in cnt_num:
+                res += cnt_num[cnt]
+            if len(res) >= k: break
+        
         return res
+    
     
 # Time: O(N)
 # Space: O(N)
