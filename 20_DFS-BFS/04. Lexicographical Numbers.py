988c33d1a874605aff1f2d1032e509e05b465584
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-07-31 16:38:50 +0530

    Update 04. Lexicographical Numbers.py

--- 20_DFS-BFS/04. Lexicographical Numbers.py
+++ 20_DFS-BFS/04. Lexicographical Numbers.py
@@ -5,12 +5,47 @@ Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
 
 Basice idea: think about Trie. we append digits to the specific suffix until we reach the target number.
 '''
+
+'''
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.val = 0
+
+class Solution:
+    def __init__(self):
+        self.root = Node()
+    
+    def addNode(self, s):
+        cur = self.root
+        for c in s:
+            if c not in cur.children:
+                cur.children[c] = Node()
+            cur = cur.children[c]
+        cur.val = int(s)
+
+    def lexicalOrder(self, n: int) -> List[int]:
+        for i in range(1, n+1):
+            self.addNode(str(i))
+
+        res = []
+        def dfs(root):
+            for c in root.children:
+                node = root.children[c]
+                res.append(node.val)
+                dfs(node)
+
+        dfs(self.root)
+        return res
+'''
+
+
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         # recursively use Trie
         res = []
-        if n < 10:
-            return list(range(1, n+1))
+        # if n < 10:
+        #     return list(range(1, n+1))
         for i in range(1, 10):
             res += [i]
             res += self.helper(i, n)
