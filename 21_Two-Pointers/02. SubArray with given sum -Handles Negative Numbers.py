4a1a40af9be2e8566ef65a853d3e2ffc24804ad6
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2023-08-02 01:04:31 +0530

    Update 02. SubArray with given sum -Handles Negative Numbers.py

--- 21_Two-Pointers/02. SubArray with given sum -Handles Negative Numbers.py
+++ 21_Two-Pointers/02. SubArray with given sum -Handles Negative Numbers.py
@@ -1,1 +1,16 @@
-# 
+# https://practice.geeksforgeeks.org/problems/subarray-range-with-given-sum0128/1
+
+import collections
+
+class Solution:
+    def subArraySum(self,arr, n, sum):
+        s = res = 0
+        cnt = collections.Counter()
+        cnt[0] = 1
+        for i in arr:
+            s += i
+            k = s-sum
+            res += cnt[k]
+            cnt[s] += 1
+        return res
+        
