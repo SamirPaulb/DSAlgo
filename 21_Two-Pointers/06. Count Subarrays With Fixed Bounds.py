40b6e2d1439c2d7ca52e091d2b1973ddfb1f02d6
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-08-02 14:45:43 +0530

    Update 06. Count Subarrays With Fixed Bounds.py

--- 21_Two-Pointers/06. Count Subarrays With Fixed Bounds.py
+++ 21_Two-Pointers/06. Count Subarrays With Fixed Bounds.py
@@ -2,33 +2,26 @@
 
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
-        ans = 0
-        r = 0 # right of window
-        l = 0 # left of window
-        n = len(nums)
-        recentMin = -1 # most recent instance of the max
-        recentMax = -1 # most recent instance of the min
-        while r < n:
-            while r<n: # sliding to the right
-                num = nums[r]
-                if not minK<=num<=maxK: # if this number is too big or small we can no longer use this window
-                    break
-                # now update the recent min and max if needed
-                if num == minK:
+        res = 0
+        l = r = 0   # window size
+        recentMin = recentMax = -1  # most recent instance of the minK and maxK
+        while r < len(nums):
+            # if any num in window is bigger or smaller ignore the whole window
+            while r < len(nums) and minK <= nums[r] <= maxK:
+                if nums[r] == minK:
                     recentMin = r
-                if num == maxK:
+                if nums[r] == maxK:
                     recentMax = r
-                r += 1
                 if recentMin != -1 and recentMax != -1:
-                    ans += (min(recentMin,recentMax)-l)+1 # we can make as many new subarrays as the earliest occurrence of recentMin or recentMax minus the starting position of our window
-            # reset everything
-            r+=1
-            l = r
-            recentMin = -1
-            recentMax = -1
-            
-        return ans
-    
+                    # we can make as many new subarrays as the earliest occurrence of recentMin or recentMax minus the starting position of our window
+                    res += min(recentMin, recentMax) - l + 1
+                r += 1
+            recentMin = recentMax = -1
+            l = r+1
+            r += 1
+        
+        return res
+
     
 # Time: O(N)
 # Space: O(1)
