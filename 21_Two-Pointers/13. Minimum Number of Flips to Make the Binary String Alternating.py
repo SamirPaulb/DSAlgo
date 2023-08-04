f7b4c8dd4d227b9e39e25f1c6da72936ede7596f
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2023-08-04 20:48:02 +0530

    Update 13. Minimum Number of Flips to Make the Binary String Alternating.py

--- 21_Two-Pointers/13. Minimum Number of Flips to Make the Binary String Alternating.py
+++ 21_Two-Pointers/13. Minimum Number of Flips to Make the Binary String Alternating.py
@@ -4,32 +4,29 @@
 class Solution:
     def minFlips(self, s: str) -> int:
         n = len(s)
-        s = s + s
+        s += s
         alt1, alt2 = "", ""
+        
         for i in range(len(s)):
             alt1 += '1' if i%2 else '0'
             alt2 += '0' if i%2 else '1'
         
-        diff1, diff2, res = 0, 0, n
-        l = 0
-        for r in range(len(s)):
-            if s[r] != alt1[r]: 
-                diff1 += 1
-            if s[r] != alt2[r]:
-                diff2 += 1
-                
-            if r-l+1 > n:
-                if s[l] != alt1[l]: 
-                    diff1 -= 1
-                if s[l] != alt2[l]:
-                    diff2 -= 1
-                l += 1
-            
-            if r-l+1 == n: 
-                res = min(res, diff1, diff2)
+        diff1 = diff2 = 0
+        for i in range(n):
+            if s[i] != alt1[i]: diff1 += 1
+            if s[i] != alt2[i]: diff2 += 1
         
-        return res
-    
+        l = 0
+        res = min(diff1, diff2)
+        for r in range(n, len(s)):
+            if s[r] != alt1[r]: diff1 += 1
+            if s[r] != alt2[r]: diff2 += 1
+            if s[l] != alt1[l]: diff1 -= 1
+            if s[l] != alt2[l]: diff2 -= 1
+            l += 1
+            res = min(res, diff1, diff2)
+        
+        return res    
     
     
 # Time: O(N)
