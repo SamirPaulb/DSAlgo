09ad9df4fe779257004ff83fd7636521db57e85d
Srujithchaithanya1 <84473285+Srujithchaithanya1@users.noreply.github.com>
2023-10-31 22:58:02 +0530

    Update bubblesort.py
    updated bubble sort code with slightly efficient code by adding additional flag variable

--- 03_Sorting-Algorithms/bubblesort.py
+++ 03_Sorting-Algorithms/bubblesort.py
@@ -1,14 +1,20 @@
-# Bubble Sort Algorithm
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        # Flag to optimize by breaking the loop if no swaps are performed in an iteration
+        swapped = False
+        
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                # Swap the elements
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        
+        # If no two elements were swapped in the inner loop, the array is already sorted
+        if not swapped:
+            break
 
-arr = [12,34,56,8,45,8,3,7,23,7,975,345,7,5,43]
-
-for i in range(len(arr)):
-    for j in range(len(arr)-i-1):
-        if arr[j] > arr[j+1]:
-            arr[j], arr[j+1] = arr[j+1], arr[j]
-
-
-print(arr)
-
-# Time: O(N^2)
-# Space: O(1)
+# Example usage:
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr)
+print("Sorted array is:", arr)
