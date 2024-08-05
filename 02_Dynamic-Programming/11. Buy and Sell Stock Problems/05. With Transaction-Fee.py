24568fe32b4c8e0d7b1fef732c066d2e6db2dd67
SamirPaulb <77569653+SamirPaulb@users.noreply.github.com>
2024-08-05 23:34:43 +0530

    Update 05. With Transaction-Fee.py

--- 02_Dynamic-Programming/11. Buy and Sell Stock Problems/05. With Transaction-Fee.py
+++ 02_Dynamic-Programming/11. Buy and Sell Stock Problems/05. With Transaction-Fee.py
@@ -1,21 +1,19 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+# Same as "02. As Many Transactions As You Like.py" with transaction fee
 
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        obsp = - prices[0]   # Old Brought State Profite
-        ossp = 0             # Old Sold State Profite
-        
-        for i in range(1, len(prices)):
-            nbsp = 0         # New Brought State Profit
-            nssp = 0         # New Sold State Profit
-            
-            nbsp = max(ossp - prices[i], obsp)
-            nssp = max(obsp + prices[i] - fee, ossp)
-            
-            obsp = nbsp
-            ossp = nssp
-        
-        return ossp
+        memo = {}
+        def solve(i,can_sell):
+            if i == len(prices): return 0
+            if (i,can_sell) in memo: return memo[(i,can_sell)]
+            if can_sell == 1:
+                profit = max(prices[i] + solve(i+1,0) - fee, solve(i+1,1))
+            else:
+                profit = max(-prices[i] + solve(i+1,1), solve(i+1,0))
+            memo[(i,can_sell)] = profit
+            return profit
+        return solve(0,0)
 
-# Time: O(n)
-# Space: O(1)
+# Time: O(2*N)
+# Space: O(2*N)
